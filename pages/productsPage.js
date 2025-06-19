b5f5585e377c2d77aec2725d422a321d86c95a39
rinku-rin <paramvirsinghqa@gmail.com>
2025-06-19 16:26:39 -0400

    added data driven user login data and testcase to handle ddt

---
+++ pages/productsPage.js
@@ -0,0 +1,8 @@
+export class ProductsPage {
+  constructor (page){
+    this.page = page;
+    this.URL = 'https://www.saucedemo.com/inventory.html'
+
+  }
+}
+
--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,17 +1,35 @@
 import {test, expect} from '@playwright/test'
 import { LoginPage } from '../pages/loginPage';
+import { ProductsPage } from '../pages/productsPage';
 
 
+const loginTestData = JSON.parse(JSON.stringify(require("../data/loginTestData.json"))); 
 const base_URL = 'https://www.saucedemo.com/';
+let loginPage;
+let productsPage;
+
+test.beforeAll( async({page})=> {
+  loginPage = new LoginPage(page);
+  productsPage = new ProductsPage(page);
+})
 
 test('Standard Login Test', async ({page}) => {
-  const loginPage = new LoginPage(page);
-  
   await page.goto(base_URL);
-
   await loginPage.login('standard_user', 'secret_sauce');
-
-
-
-
+  expect(page.url()).toBe(productsPage.URL);
 })
+
+test.describe('Data driven user login test', function() {
+
+  for(let data of loginTestData) {
+
+    test(`Login test for ${data.id}`, async ({ page }) => {
+      await page.goto(base_URL);
+      await loginPage.login(data.username, data.password);
+      expect(page.url()).toBe(productsPage.URL);
+    })
+  }
+
+
+
+});
